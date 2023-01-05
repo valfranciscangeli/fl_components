f49c2d2b51f18c6e238597f81cdf78ba74d0473c
Valeria Franciscangeli <vvfranciscangeli@gmail.com>
2023-01-05 20:30:00 -0300

    hs clase 69

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Componentes en Flutter'),
+          elevation: 0,
+        ),
+        body: ListView.separated(
+            itemBuilder: (context, index) => ListTile( 
+              leading: const Icon(Icons.access_time_outlined),
+               title: const Text('Nombre de ruta'),
+               onTap: () {
+                 
+               },),
+            separatorBuilder: (_, __) => const Divider(),
+            itemCount: 10
+            )
+            );
+  }
+}
