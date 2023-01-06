31cd0ae8097b385863e693247f492dd6bbe29479
Valeria Franciscangeli <vvfranciscangeli@gmail.com>
2023-01-05 21:50:09 -0300

    fin clase 73

--- lib/router/app_routes.dart
+++ lib/router/app_routes.dart
@@ -1,18 +1,49 @@
+import 'package:fl_components/models/models.dart';
 import 'package:fl_components/screens/screens.dart';
 import 'package:flutter/material.dart';
 
 class AppRoutes {
   static const initiaRoute = 'home';
-  static Map<String, Widget Function(BuildContext)> routes = {
-        'home': (BuildContext context) => const HomeScreen(),
-        'listview1': (BuildContext context) => const ListView1Screen(),
-        'listview2': (BuildContext context) => const ListView2Screen(),
-        'alert': (BuildContext context) => const AlertScreen(),
-        'card': (BuildContext context) => const CardScreen(),
-      }
+  static final menuOptions = <MenuOption>[
+    // TODO: borrar home
+    MenuOption(
+        route: 'home',
+        name: 'Home Screen',
+        screen: const HomeScreen(),
+        icon: Icons.home),
+    MenuOption(
+        route: 'listview1',
+        name: 'List View Tipo 1',
+        screen: const ListView1Screen(),
+        icon: Icons.list),
+    MenuOption(
+        route: 'listview2',
+        name: 'List View Tipo 2',
+        screen: const ListView2Screen(),
+        icon: Icons.list),
+    MenuOption(
+        route: 'alert',
+        name: 'Alertas - Alerts ',
+        screen: const AlertScreen(),
+        icon: Icons.notifications),
+    MenuOption(
+        route: 'cars',
+        name: 'Tarjetas - Cards',
+        screen: const CardScreen(),
+        icon: Icons.credit_card),
+  ];
 
-    static Route<dynamic> onGenerateRoute (RouteSettings settings) {
-        return MaterialPageRoute(
-          builder: (context) => const AlertScreen(),
-        );
+  static Map<String, Widget Function(BuildContext)> routes = {
+    'home': (BuildContext context) => const HomeScreen(),
+    'listview1': (BuildContext context) => const ListView1Screen(),
+    'listview2': (BuildContext context) => const ListView2Screen(),
+    'alert': (BuildContext context) => const AlertScreen(),
+    'card': (BuildContext context) => const CardScreen(),
+  };
+
+  static Route<dynamic> onGenerateRoute(RouteSettings settings) {
+    return MaterialPageRoute(
+      builder: (context) => const AlertScreen(),
+    );
+  }
 }
