6046f2dddbe8bbac780ad401ac015f86f7337815
Valeria Franciscangeli <vvfranciscangeli@gmail.com>
2023-01-06 04:32:19 -0300

    fin clase 83

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -1,9 +1,39 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AlertScreen extends StatelessWidget {
   const AlertScreen({Key? key}) : super(key: key);
 
-  void displayDialog(BuildContext context) {
+  void displayDialogIOS(BuildContext context) {
+    showCupertinoDialog(
+        barrierDismissible: true,
+        context: context,
+        builder: (context) {
+          return CupertinoAlertDialog(
+            title: const Text('Titulo'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: const [
+                Text('Este es el contenido de la alerta'),
+                SizedBox(height: 10),
+                FlutterLogo(size: 100)
+              ],
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text('Cancelar')),
+              TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text('Ok')),
+            ],
+          );
+        });
+  }
+
+  void displayDialogAndroid(BuildContext context) {
     showDialog(
         barrierDismissible: true,
         context: context,
@@ -28,7 +58,10 @@ class AlertScreen extends StatelessWidget {
             actions: [
               TextButton(
                   onPressed: () => Navigator.pop(context),
-                  child: const Text('Cancelar'))
+                  child: const Text('Cancelar')),
+              TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text('Ok')),
             ],
           );
         });
@@ -46,7 +79,9 @@ class AlertScreen extends StatelessWidget {
             style: TextStyle(fontSize: 20),
           ),
         ),
-        onPressed: () => displayDialog(context),
+        onPressed: () => Platform.isAndroid
+        ? displayDialogAndroid(context)
+        : displayDialogIOS(context),
       )),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.close),
