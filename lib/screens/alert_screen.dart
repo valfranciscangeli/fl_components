5e2a17660e7137fd0263d44832b7ec942d4f331a
Valeria Franciscangeli <vvfranciscangeli@gmail.com>
2023-01-06 04:18:44 -0300

    fin clase 82

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -1,14 +1,58 @@
 import 'package:flutter/material.dart';
 
 class AlertScreen extends StatelessWidget {
-   
   const AlertScreen({Key? key}) : super(key: key);
-  
+
+  void displayDialog(BuildContext context) {
+    showDialog(
+        barrierDismissible: true,
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+            elevation: 5,
+            title: const Text('Titulo'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: const [
+                Text('Este es el contenido de la alerta'),
+                SizedBox(
+                  height: 10,
+                ),
+                FlutterLogo(
+                  size: 100,
+                )
+              ],
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text('Cancelar'))
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-         child: Text('AlertScreen'),
+          child: ElevatedButton(
+        child: const Padding(
+          padding: EdgeInsets.all(10),
+          child: Text(
+            'Mostrar alerta',
+            style: TextStyle(fontSize: 20),
+          ),
+        ),
+        onPressed: () => displayDialog(context),
+      )),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.close),
+        onPressed: () {
+          Navigator.pop(context);
+        },
       ),
     );
   }
--- lib/themes/app_theme.dart
+++ lib/themes/app_theme.dart
@@ -5,27 +5,38 @@ class AppTheme {
   static const Color accent = Colors.pinkAccent;
 
   static final ThemeData lightTheme = ThemeData.light().copyWith(
-      primaryColor: primary,
-      // Appbar
-      appBarTheme: const AppBarTheme(
-        color: primary,
-        elevation: 0,
-      ),
-      // TextButton Theme
-      textButtonTheme: TextButtonThemeData(
+    primaryColor: primary,
+    // Appbar
+    appBarTheme: const AppBarTheme(
+      color: primary,
+      elevation: 0,
+    ),
+    // TextButton Theme
+    textButtonTheme: TextButtonThemeData(
         style: TextButton.styleFrom(foregroundColor: primary)
+    ),
+    // Floating action buttons theme
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: primary,
+      elevation: 5,
+    ),
+    // Elevated button theme
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: accent,
+        elevation: 10, 
+        shape: const StadiumBorder(),
       )
-      );
+    )
+  );
 
   static final ThemeData darkTheme = ThemeData.dark().copyWith(
-      primaryColor: primary,
-      // Appbar
-      appBarTheme: const AppBarTheme(
-        color: primary,
-        elevation: 0,
-      ),
-      scaffoldBackgroundColor: Colors.black,
-      );
-
-
+    primaryColor: primary,
+    // Appbar
+    appBarTheme: const AppBarTheme(
+      color: primary,
+      elevation: 0,
+    ),
+    scaffoldBackgroundColor: Colors.black,
+  );
 }
